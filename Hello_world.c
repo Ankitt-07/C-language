993887aa2f8dd355f58dea921123629e004e4a85
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-04-03 23:56:02 +0530

    Update Hello_world.c

--- Hello_world.c
+++ Hello_world.c
@@ -2,7 +2,7 @@ include<stdio.h>
 include<conio.h>
 
 int main() {
-   /// printf() displays the string inside quotation
+   /// printf() displays the string inside quotationn
    printf("Hello, World!");
    return 0;
 }
