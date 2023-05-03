6fa79e8abf41cf8ea28851df8603650de259d3bf
Ankitt singh <88707094+Ankitt-07@users.noreply.github.com>
2023-05-04 00:34:09 +0530

    Update Activity_Selection.c

--- Activity_Selection.c
+++ Activity_Selection.c
@@ -2,7 +2,7 @@
 int main(void){
    int start[] = {1, 3, 0, 5, 8, 5};
    int finish[] = {2, 4, 6, 7, 9, 9};
-   int activities = sizeof(start)/sizeof(start[0]);l
+   int activities = sizeof(start)/sizeof(start[0]);
    int i, j;
    printf ("Following activities are selected :-");
    i = 0;
